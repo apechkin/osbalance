6f907c4f6b8c2cd9da6f77114798d1f6888b9690
Anatoliy Pechkin <anatmozg@mail.ru>
2019-02-08 18:21:39 +0300

    initial commit

---
+++ src/components/Layout/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled('div')({
+  padding: 40,
+  background: '#f7df1e',
+  textAlign: 'center'
+})
+
+export default function Footer(props) {
+  return (
+    <Wrapper/>
+  )
+}
